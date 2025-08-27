6aaf434cac1bc23712fcbb14ac4a7df3e66676fb
Javis202530 <javisbbombo@gmail.com>
2025-08-27 21:17:40 +0300

    Create Blog.jsx

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Blog() {
+  return (
+    <div className="p-8">
+      <h2 className="text-3xl font-semibold text-pink-600">Our Blog</h2>
+      <p className="mt-2 text-gray-700">
+        Read inspiring stories, updates about our foundation, and learn how your support is transforming lives.
+      </p>
+    </div>
+  );
+}
+
+export default Blog;
