d0928b2869b3f33a21329eb72cde9688267108d1
Javis202530 <javisbbombo@gmail.com>
2025-08-27 21:20:31 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-blue-700">Welcome to River of Hope Foundation</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Together, we can change lives. Explore our crafts, support us, and join our mission to bring hope and smiles to children.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
