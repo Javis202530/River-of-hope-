5ec3a6ea1d12fae9c7adbc456001e289857e3b60
Javis202530 <javisbbombo@gmail.com>
2025-08-28 10:13:25 +0300

    Update App.jsx
    Eh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
-
 import Artisticwork from "./pages/Artisticwork";
 import Bangles from "./pages/Bangles";
 import Crafts from "./pages/Crafts";
@@ -17,17 +18,7 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-100 flex flex-col">
         {/* Navbar */}
-        <nav className="bg-blue-700 p-4 text-white flex gap-4">
-          <Link to="/" className="hover:underline">Home</Link>
-          <Link to="/about" className="hover:underline">About</Link>
-          <Link to="/blog" className="hover:underline">Blog</Link>
-          <Link to="/artisticwork" className="hover:underline">Artistic Work</Link>
-          <Link to="/bangles" className="hover:underline">Bangles</Link>
-          <Link to="/crafts" className="hover:underline">Crafts</Link>
-          <Link to="/tshirts" className="hover:underline">T-shirts</Link>
-          <Link to="/capes" className="hover:underline">Caps</Link>
-          <Link to="/contact" className="hover:underline">Contact</Link>
-        </nav>
+        <Navbar />
 
         {/* Page Content */}
         <div className="p-6 flex-1">
