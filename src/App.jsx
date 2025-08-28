0dc668276fc043f8c8f2b6668e770ad320fbbc28
Javis202530 <javisbbombo@gmail.com>
2025-08-28 10:38:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
